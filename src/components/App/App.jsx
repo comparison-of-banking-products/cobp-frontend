fc4f6b8be5a66322fd36a50641696af997085aeb
Paul <paul.ivanov1990@gmail.com>
2023-11-24 17:56:40 +0300

    feat: init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import Main from '../Main/Main';
+
+function App() {
+	return (
+		<div className="page">
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PageNotFound.css';
+
+function PageNotFound() {
+	return <h2>PageNotFound</h2>;
+}
+
+export default PageNotFound;
