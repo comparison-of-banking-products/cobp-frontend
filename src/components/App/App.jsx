9b564c1dd307c3f8fc65b67094fb336ce96c4aab
Paul <paul.ivanov1990@gmail.com>
2023-12-01 00:05:19 +0300

    feat: start routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import PageNotFound from '../PageNotFound/PageNotFound';
 import Main from '../Main/Main';
+import Deposits from '../../pages/Deposits/Deposits';
+import Credits from '../../pages/Credits/Credits';
+import Calculator from '../../pages/Calculator/Calculator';
+import Exchanges from '../../pages/Exchanges/Exchanges';
 
 function App() {
 	return (
 		<div className="page">
 			<Routes>
 				<Route path="/" element={<Main />} />
+				<Route path="/deposits" element={<Deposits />} />
+				<Route path="/credits" element={<Credits />} />
+				<Route path="/calculator" element={<Calculator />} />
+				<Route path="/exchanges" element={<Exchanges />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</div>
