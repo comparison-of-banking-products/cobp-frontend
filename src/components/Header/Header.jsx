1a6fe08cf386654f8b4af7bca5961d8c2a95551f
Paul <paul.ivanov1990@gmail.com>
2023-12-01 21:49:18 +0300

    fix: fix setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+function Header() {
+	return <Navigation />;
+}
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import SearcForm from './SearchForm/SearchForm';
+import PageNotFound from './PageNotFound/PageNotFound';
+
+export { Footer, Header, SearcForm, PageNotFound };
