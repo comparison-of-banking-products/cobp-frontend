536708bea430ec706a4f4c4ad8720d62b3016af0
Nikita Aleksankin <NTL5TIA@gmail.com>
2023-12-05 23:28:18 +0300

    feat: работа с SCSS: Добавил шрифты, переменные, миксины

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,5 @@
+function Calculator() {
+	return <h2 className="title">калькулятор</h2>;
+}
+
+export default Calculator;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Calculator from '../Calculator/Calculator';
 
 function Main() {
-	return <h1 className="main__heading">Main</h1>;
+	return (
+		<>
+			<h1 className="main__heading">Main</h1>
+			<Calculator />
+		</>
+	);
 }
 
 export default Main;
