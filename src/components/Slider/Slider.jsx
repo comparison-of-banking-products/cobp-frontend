9d373229992285e1677ac2b30133040f0f09ff41
Paul <paul.ivanov1990@gmail.com>
2023-12-07 13:14:10 +0300

    feat: feature Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+import '../../scss/components/slider.scss';
 import { Button } from '../index';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 function Slider() {
 	const handleClick = () => {
@@ -7,12 +12,34 @@ function Slider() {
 	};
 
 	return (
-		<Button
-			type="button"
-			btnClass="button__primary button__primary_sm"
-			textBtn="подобрать кредит"
-			onClick={handleClick}
-		/>
+		<section className="slider">
+			<Swiper
+				spaceBetween={30}
+				centeredSlides={true}
+				autoplay={{
+					delay: 4000,
+					disableOnInteraction: false,
+					pauseOnMouseEnter: true,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Autoplay, Pagination]}
+				className="slider"
+			>
+				<SwiperSlide>
+					<Button
+						type="button"
+						btnClass="button__primary button__primary_sm"
+						textBtn="подобрать кредит"
+						onClick={handleClick}
+					/>
+				</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+			</Swiper>
+		</section>
 	);
 }
 
