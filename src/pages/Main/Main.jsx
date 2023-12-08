ea5bea156ef43c02273741ea027e7683b74b52f9
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-12-08 20:54:15 +0300

    fix conf dev 2

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProductCards from '../../components/ProductCards/ProductCards'
+import { Slider } from '../../components';
 
 function Main() {
-    return (
-        <>
-            <h1 className="main__heading">Main</h1>;
+	return (
+		<>
+			<h1>Пока какой-то текст</h1>
+			<Slider />
             <ProductCards />
-        </>
-    );
+		</>
+	);
 }
 
 export default Main;
