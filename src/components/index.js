8c103dbed7706385ea38d99208a18e43cf72ae32
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-12-08 21:49:19 +0300

    ready for brunch

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import SearcForm from './SearchForm/SearchForm';
 import PageNotFound from './PageNotFound/PageNotFound';
+import ProductCards from './ProductCards/ProductCards';
 
-export { Footer, Header, SearcForm, PageNotFound };
+export { Footer, Header, SearcForm, PageNotFound, ProductCards };
