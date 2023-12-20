208cd7c641fe4070baa29e6b12188046a34a9bab
Karina Savilova <savilovakarina@yandex.ru>
2023-12-20 20:17:38 +0200

    feat/Добавлен компонент depositsCardList

---
+++ src/components/DepositsCardList/DepositsCardList.jsx
@@ -0,0 +1,27 @@
+import { Button } from '../index';
+import depositsImg from '../../images/icons/diposit__arrow.svg';
+
+function DepositsCardList() {
+	return (
+		<section aria-label="Вклады" className="deposits">
+			<div className="deposits__filter">
+				{/* это селектр */}
+				<p>по ставке</p>
+				<button className="deposits__button">
+					{/* <img className="deposits__img" src={depositsImg} alt="-" /> */}
+				</button>
+			</div>
+
+			{/* карточки вкладов */}
+
+			<Button
+				type="button"
+				btnClass={'deposits__button-more'}
+				textBtn={'Показать еще'}
+				// onClick={onClick} disabled={disabled}
+			></Button>
+		</section>
+	);
+}
+
+export default DepositsCardList;
