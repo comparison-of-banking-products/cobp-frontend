01ab2665161dfec343c8c618ba1bf5fbdecf16fa
Karina Savilova <savilovakarina@yandex.ru>
2023-12-20 20:44:55 +0200

    feat/Добавлены отступы

--- src/components/ProductCards/ProductCards.jsx
+++ src/components/ProductCards/ProductCards.jsx
@@ -6,25 +6,25 @@ function ProductCards() {
 		<section className="product-cards">
 			<h2 className="product-cards__title">Наши продукты</h2>
 			<div className="product-cards__container">
-				<ProdCard 
+				<ProdCard
 					className={'prod-card__wide'}
 					title={'Кредиты'}
 					text={'Удобный расчет на калькуляторе и оформление онлайн'}
 				/>
 
-				<ProdCard 
+				<ProdCard
 					className={'prod-card__slim'}
 					title={'Кредитные карты'}
 					text={'Подходит для повседневных трат и покупок в рассрочку'}
 				/>
 
-				<ProdCard 
+				<ProdCard
 					className={'prod-card__slim'}
 					title={'Дебетовые карты'}
 					text={'Кэшбэк рублями до 30%, переводы без комиссии'}
 				/>
 
-				<ProdCard 
+				<ProdCard
 					className={'prod-card__wide'}
 					title={'Вклады'}
 					text={'Откройте вклад с пополнением, каждый месяц получайте проценты на карту или вклад'}
