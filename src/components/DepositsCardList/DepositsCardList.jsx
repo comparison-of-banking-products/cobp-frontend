de6ae22bf35d4c9713bc24d07a0e9b23fa01a76f
Karina Savilova <savilovakarina@yandex.ru>
2023-12-21 18:50:05 +0200

    Добавила на страницу селектор

--- src/components/DepositsCardList/DepositsCardList.jsx
+++ src/components/DepositsCardList/DepositsCardList.jsx
@@ -1,24 +1,37 @@
+import { useState } from 'react';
 import { Button } from '../index';
 
 function DepositsCardList() {
+	const [selectedOption, setSelectedOption] = useState('по ставке');
+
+	const handleSelectChange = (event) => {
+		setSelectedOption(event.target.value);
+	};
+
+	const options = [
+		{ value: 'по ставке', label: 'По ставке' },
+		{ value: 'По сроку', label: 'По сроку' },
+		{ value: 'По доходу', label: 'По доходу' },
+		{ value: 'По сумме', label: 'По сумме' },
+	];
+
 	return (
 		<section aria-label="Вклады" className="deposits">
 			<div className="deposits__filter">
-				{/* это селектр */}
-				<p>по ставке</p>
-				<button className="deposits__button">
-					{/* <img className="deposits__img" src={depositsImg} alt="-" /> */}
-				</button>
+				<select value={selectedOption} onChange={handleSelectChange} className="deposits__select">
+					{options.map((option, index) => (
+						<option key={index} value={option.value}>
+							{option.label}
+						</option>
+					))}
+				</select>
+
+				<button className="deposits__button"></button>
 			</div>
 
 			{/* карточки вкладов */}
 
-			<Button
-				type="button"
-				btnClass={'deposits__button-more'}
-				textBtn={'Показать еще'}
-				// onClick={onClick} disabled={disabled}
-			></Button>
+			<Button type="button" btnClass={'deposits__button-more'} textBtn={'Показать еще'}></Button>
 		</section>
 	);
 }
