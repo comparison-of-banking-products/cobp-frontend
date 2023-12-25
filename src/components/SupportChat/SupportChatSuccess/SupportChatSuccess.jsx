9643e84ed19a52af3e047e4b7a6ebf58fb15b420
Paul <paul.ivanov1990@gmail.com>
2023-12-26 00:36:16 +0300

    feat: upd support chat form

--- src/components/SupportChat/SupportChatSuccess/SupportChatSuccess.jsx
+++ src/components/SupportChat/SupportChatSuccess/SupportChatSuccess.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 function SupportChatSuccess() {
 	return (
 		<div className="support-chat-form__success">
-			<h2 className="support-chat-form__title-success">Вопрос успешно отправлен</h2>
-			<p className="support-chat-form__subtitle-success">
-				Наша команда уже работает над тем, чтобы вы как можно скорее получили ответ.
-			</p>
+			<h2 className="support-chat-form__title-success">Вопрос отправлен</h2>
+			<p className="support-chat-form__subtitle-success">Ожидайте ответа на указанную вами почту</p>
 		</div>
 	);
 }
