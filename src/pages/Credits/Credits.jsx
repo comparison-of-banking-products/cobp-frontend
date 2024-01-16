240b415a087ca8c6a331afff6875dd64c20b28e2
Paul <paul.ivanov1990@gmail.com>
2024-01-16 22:57:32 +0300

    feat: promo for deposits and credits

--- src/pages/Credits/Credits.jsx
+++ src/pages/Credits/Credits.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Promo } from '../../components';
 
 function Credits() {
-	return <div>Credits</div>;
+	return (
+		<>
+			<Promo
+				title="Подбор кредита"
+				subtitle="Подберём банки, которые готовы выдать кредит по выгодной для вас ставке"
+			/>
+		</>
+	);
 }
 
 export default Credits;
--- src/pages/Deposits/Deposits.jsx
+++ src/pages/Deposits/Deposits.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
-import { PromoDeposits, DepositsCardList, DepositFilter } from '../../components';
+import { Promo, DepositsCardList, DepositFilter } from '../../components';
 
 function Deposits() {
 	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	return (
 		<>
-			<PromoDeposits />
-			<DepositFilter isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted} />
-			<DepositsCardList isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted} />
+			<Promo
+				deposits
+				title="Вклады c&nbsp;высоким процентом"
+				subtitle="Сервис сравнения банковских депозитов позволяет мгновенно найти лучшие предложения
+					по&nbsp;вкладам с&nbsp;выгодными условиями и&nbsp;высокой процентной ставкой"
+			/>
+			{/* <DepositFilter isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted} /> */}
+			{/* <DepositsCardList isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted} /> */}
 		</>
 	);
 }
