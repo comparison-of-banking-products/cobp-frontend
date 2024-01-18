67d59aae7e30ad7369dce6dd9beed9af2ff004de
Paul <paul.ivanov1990@gmail.com>
2024-01-19 00:25:01 +0300

    feat: fix

--- src/components/ExchangeRate/ExchangeRate.jsx
+++ src/components/ExchangeRate/ExchangeRate.jsx
@@ -1,19 +1,28 @@
 function ExchangeRate({ valute }) {
+	if (!valute) {
+		return null;
+	}
+
 	let imageRates = 'exchange-rate__difference-image_no-changes';
 
-	if (valute.diff > 0) {
+	if (valute.diff.toFixed(2) > 0) {
 		imageRates = 'exchange-rate__difference-image_up';
-	} else if (valute.diff < 0) {
+	} else if (valute.diff.toFixed(2) < 0) {
 		imageRates = 'exchange-rate__difference-image_down';
 	}
+
+	const roundedDiff = valute.diff.toFixed(2);
+	const displayDiff = Math.abs(roundedDiff) === 0 ? roundedDiff.slice(1) : roundedDiff;
 
 	return (
 		<>
 			<li className="exchange-rate__currency-item">
 				<p className="exchange-rate__difference">
 					{`${valute.base}
-            ${valute.rate.toFixed(2).replace('.', ',')}
-            ${valute.diff.toFixed(2).replace('.', ',')}`}
+            ${valute.rate.toFixed(2).replace('.', ',')}`}
+					&nbsp;
+					{`
+            ${displayDiff.replace('.', ',')}`}
 				</p>
 				<p className={`exchange-rate__difference-image ${imageRates}`} />
 			</li>
