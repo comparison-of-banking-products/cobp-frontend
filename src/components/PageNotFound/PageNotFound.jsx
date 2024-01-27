7278f057eaa36baa94667f3ee331a8d88f54d20c
Paul <paul.ivanov1990@gmail.com>
2024-01-27 22:14:35 +0300

    feat: page-404

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageNotFoundImg from '../../images/page-404.png';
 
 function PageNotFound() {
 	return (
 		<section className="page-not-found">
-			<h2 className="page-not-found__title">404</h2>
-			<p className="page-not-found__subtitle">Страница не найдена</p>
+			<img src={PageNotFoundImg} className="page-not-found__img" alt="Страница не найдена - 404" />
+			<h2 className="page-not-found__title">Страница не найдена</h2>
+			<p className="page-not-found__subtitle">
+				Страница, которую вы запрашиваете, не существует. Возможно она устарела, была удалена, или
+				был введен неверный адресс в адресной строке
+			</p>
 			<Link to="/" className="page-not-found__btn">
-				На главную
+				Перейти на главную
 			</Link>
 		</section>
 	);
