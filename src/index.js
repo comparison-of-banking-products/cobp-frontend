39e7bf019ea3addb2b5e5172af6d47e2300c8489
Paul <paul.ivanov1990@gmail.com>
2024-01-28 22:55:37 +0300

    feat: add serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -22,4 +22,17 @@ root.render(
 	</React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('/serviceWorker.js')
+			.then((registration) => {
+				console.log('Service Worker registered:', registration);
+			})
+			.catch((error) => {
+				console.error('Service Worker registration failed:', error);
+			});
+	});
+}
+
 reportWebVitals();
