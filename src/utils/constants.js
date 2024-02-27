5f38ca1ed1b136176bfbeb63c9e40bbaba9d2385
elicrock <rockelic@yandex.ru>
2024-02-27 09:25:49 +0300

    fix: some fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-// const BASE_URL_SUPPORT_CHAT = 'https://benchmark.com.ru/chat-api';
+const BASE_URL_SUPPORT_CHAT = 'https://benchmark.com.ru/chat-api';
 const BASE_URL_ONLINE_CHAT = 'https://benchmark.com.ru/chat-api';
 const currencyList = ['Рубли ₽', 'Доллары $', 'Евро €', 'Юани ¥'];
 const initialVisibleCount = 6;
 
-export { BASE_URL_ONLINE_CHAT, currencyList, initialVisibleCount };
+export { BASE_URL_SUPPORT_CHAT, BASE_URL_ONLINE_CHAT, currencyList, initialVisibleCount };
